017474b7cfa1b302bb6cdd3b659e6f5e49ea8ff7
CodeBulletin <48746746+CodeBulletin@users.noreply.github.com>
2021-07-05 13:32:55 +0530

    Update main.cpp

--- DSCpp/DSCpp/main.cpp
+++ DSCpp/DSCpp/main.cpp
@@ -6,19 +6,9 @@
 #include "Queue.hpp"
 #include "Stack.hpp"
 #include "Trees.hpp"
+#include "Nodes.hpp"
 
 int main() {
-	{
-		DS::AVLTree<int> BST;
-		for (int i = -2000000; i <= 2000000; i++) {
-			BST.Insert(i);
-		}
-
-		for (int i = -2000000; i <= 2000000; i++) {
-			BST.Delete(BST.Find(i));
-		}
-	}
-
 	std::cin.get();
 	return 0;
 }
